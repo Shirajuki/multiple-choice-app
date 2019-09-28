6d2faa1d4520efedf967d95715592e7768c74518
shirajuki <vulturegfd@reborn.com>
2019-09-28 22:18:24 +0200

    Initial commit

---
+++ spoersmaal2.js
@@ -0,0 +1,117 @@
+var tdt4109 = [
+{
+  "Q": "In order for something to be considered a computer it must have a keyboard attached.",
+  "A": ["true","false","only certain computers"],
+  "R": "false"
+},
+{
+  "Q": "The problem of rewriting was solved by electronic computer using",
+  "A": ["integrated circuits", "the central processing unit (CPU)", "punch cards", "storage units"],
+  "R": "the central processing unit (CPU)"
+},
+{
+  "Q": "When using Google or Bing, what keyword can be used to provide you with a definition of a certain term?",
+  "A": ["look-up","define","dictionary","definition"],
+  "R": "define"
+},
+{
+  "Q": "What word is interchangeable with computer",
+  "A": ["processor","desktop","personal","intergrated circuit (IC)"],
+  "R": "processor"
+},
+{
+  "Q": "What is educational background is required to write algorithms?",
+  "A": ["BS in computer science or computer engineering","MS in computer science or computer engineering","a high school diploma","no educational background is required"],
+  "R": "no educational background is required"
+},
+{
+  "Q": "A set of programs is called",
+  "A": ["instructions","agent","software","all of the above"],
+  "R": "software"
+},
+{
+  "Q": "How many symbols can be represented by 9 bits?",
+  "A": ["256","512","1024","2048"],
+  "R": "512"
+},
+{
+  "Q": "PandA representation is what kind of system?",
+  "A": ["decimal","binary","hexadecimal","byte"],
+  "R": "binary"
+},
+{
+  "Q": "Extended ASCII is",
+  "A": ["7 bit code","8 bit code","2 bit code","10 bit code"],
+  "R": "8 bit code"
+},
+{
+  "Q": "This defines how characters relate to each other when they are compared.",
+  "A": ["digitizing","binary sequence","collating sequence","information representation"],
+  "R": "collating sequence"
+},
+{
+  "Q": "When using physical phenomena to encode information, name on potential solution if there are more than two alternatives.",
+  "A": ["there is no solution","adopt them all as present","adopt one as present and all the other alternatives as absent","adopt them all as absent"],
+  "R": "adopt one as present and all the other alternatives as absent"
+},
+{
+  "Q": "Information describing information is called",
+  "A": ["special information","metadata","special-data","formatting"],
+  "R": "metadata"
+},
+{
+  "Q": "K bits in a sequence yield how many symbols?",
+  "A": ["K**2","2**K","K","2"],
+  "R": "2**K"
+},
+{
+  "Q": "A machine instructions uses how many bytes of memory?",
+  "A": ["1","2","4","128"],
+  "R": "1"
+},
+{
+  "Q": "An object code is",
+  "A": ["programming language","intermediate code.","machine language","assembly language"],
+  "R": "machine language"
+},
+{
+  "Q": "CPU is also referred to as",
+  "A": ["Processor","Microprocessor","Core","All of the above"],
+  "R": "All of the above"
+},
+{
+  "Q": "Which of the following is used for input and output?",
+  "A": ["keyboard","hard disk","mouse","printer"],
+  "R": "hard disk"
+},
+{
+  "Q": "The processor has how many moving parts?",
+  "A": ["too many to count","8","2","0"],
+  "R": "0"
+},
+{
+  "Q": "From largest to smallest, the correct order of prefixes is",
+  "A": ["giga, kilo, mega, tera","kilo, mega, giga, tera","tera, giga, mega, kilo","kilo, mega, tera, giga"],
+  "R": "tera, giga, mega, kilo"
+},
+{
+  "Q": "Modern computers know",
+  "A": ["only a few instructions","a couple dozen instructions","about a hundred instructions","thousands of instructions"],
+  "R": "thousands of instructions"
+},
+{
+  "Q": "Rerunning a program with the same data produces",
+  "A": ["different results depending on the time of day","exactly the same result every time","different results depending on which computer it is run on","the same reuslts most of the time but sometimes it is different"],
+  "R": "exactly the same result every time"
+},
+{
+  "Q": "If this.Opactiy += 1.0; increases the opacity, what line of code would decrease the opacity?",
+  "A": ["this.Opacity = 1.0;","this.Opacity -= 1.0;","this.Opactiy ++ 1.0;","this.Opacity -- 1.0;"],
+  "R": "this.Opacity -= 1.0;"
+},
+{
+  "Q": "Which of the following characteristics of a computer depends on the number of Fetch/Execute Cycles it performs per second?",
+  "A": ["memory size","price","speed","ALU"],
+  "R": "speed"
+},
+];
