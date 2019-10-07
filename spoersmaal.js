6d88e255068d77a0c3297353ee1ffd43fa1259e6
shirajuki <vulturegfd@reborn.com>
2019-10-07 07:52:11 +0200

    asd

---
+++ spoersmaal.js
@@ -0,0 +1,58 @@
+tdt4109 = [...tdt4109,
+{
+  "Q": "Hva er Alan Turing kjent for?",
+  "A": ["Han oppfant datamaskinen.", "Han utformet det matematiske grunnlaget for dagens datamaskiner.","Han startet opp Intel.", "Han bidro til forskning på transistorer."],
+  "R": "Han utformet det matematiske grunnlaget for dagens datamaskiner."
+},
+{
+  "Q": "Hva kalles mindre kretskort som plugges inn i hoved(krets-)kortet?",
+  "A": ["Grafikk-kort","RAM-brikker","Sønne-kort","Datterkort"],
+  "R": "Datterkort"
+},
+{
+  "Q": "Hva står CPU for?",
+  "A": ["Central Processing Unit","Central Power Unit","Coding Power Unit","Core Processing Unit"],
+  "R": "Central Processing Unit"
+},
+{
+  "Q": "RAM står for random access memmory, men hva mees egentlig med random i dette tilfellet?",
+  "A": ["At hukommelsescellene kan aksesseres direkte i tilfeldig rekkefølge.","At vi henter ut tilfeldige data.","At dataen kommer i tilfeldig rekkefølge.","At det er tilfeldig hvor dataen kommer fra."],
+  "R": "At hukommelsescellene kan aksesseres direkte i tilfeldig rekkefølge."
+},
+{
+  "Q": "Hvilket metall finner man inni CPUer som IT-industrien er fullstendig avhengig av?",
+  "A": ["Silisium","Silikon","Stål","Aluminium"],
+  "R": "Silisium"
+},
+{
+  "Q": "Hva kunne en CPU gjøre som var helt revolusjonerende når den kom?",
+  "A": ["Utføre operasjoner lagret i minnet","Uføre flere operasjoner på en gang","Lese digitale data","Prosessere grafisk informasjon"],
+  "R": "Utføre operasjoner lagret i minnet"
+},
+{
+  "Q": "Hva sier Moores lov?",
+  "A": ["Loven sier at klokkehastigheten øker proporsjonalt med antall programtellere.","Loven postulerer at: Ting vil gå galt uavhengig av av situasjon gitt muligheten","Loven sier at samplingsfrekvensen må være minst dobbelt så rask som den raskeste frekvensen","Loven sier at antall transistorer i en integrert krets dobles hvert andre år."],
+  "R": "Loven sier at antall transistorer i en integrert krets dobles hvert andre år."
+},
+{
+  "Q": "Hva er fotolitografi?",
+  "A": ["En teknikk for å gjøre bilder lagret digitalt til å ligne på et maleri","En teknikk for å lage PC-er ved at man først konstruerer et bilde av hvordan den skal se ut.","En teknikk for å lage transistorer i en integrert krets","En teknikk som brukes i produksjonen av lithium-batterier"],
+  "R": "En teknikk for å lage transistorer i en integrert krets"
+},
+{
+  "Q": "Hva er software?",
+  "A": ["Instruksjoner lagret i en datamaskins minne","Et program som kun gjør ufarlige operasjoner, i motsetning til hardware","En spesiell type putevar som er ekstra myk","De \"mykere\" metallene som blir brukt i en pc."],
+  "R": "Instruksjoner lagret i en datamaskins minne"
+},
+{
+  "Q": "Hva er det som benyttes i dag som har erstattet vacuum tubes (vakumrør)?",
+  "A": ["RAM","CPU","Transistorer","Assembly"],
+  "R": "Transistorer"
+},
+// 2
+// {
+//   "Q": "",
+//   "A": ["","","",""],
+//   "R": ""
+// },
+];
